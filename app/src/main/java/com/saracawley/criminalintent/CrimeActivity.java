a39f13fbd5e3d86429e90ffdaf4de8d2da3586ae
scaw76 <scawley@dmail.dixie.edu>
2016-02-19 13:23:07 -0700

    Created landscape view and added singleton data class.

--- app/src/main/java/com/saracawley/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/saracawley/criminalintent/CrimeActivity.java
@@ -1,31 +1,10 @@
 package com.saracawley.criminalintent;
 
-import android.app.Fragment;
-import android.content.Context;
-import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-        FragmentManager fm = getSupportFragmentManager();
-        android.support.v4.app.Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-        if(fragment == null){
-            fragment = new CrimeFragment();
-            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-        }
+    protected android.support.v4.app.Fragment createFragment(){
+        return new CrimeFragment();
     }
 
 }
