6eb99785379033af8866fb3329bd6fce862e904a
scaw76 <scawley@dmail.dixie.edu>
2016-04-07 15:52:12 -0600

    Ch 10 complete starting ch 14

--- app/src/main/java/com/saracawley/criminalintent/CrimeLab.java
+++ app/src/main/java/com/saracawley/criminalintent/CrimeLab.java
@@ -1,6 +1,9 @@
 package com.saracawley.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.saracawley.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +16,8 @@ public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context){
         if(sCrimeLab == null){
@@ -21,6 +26,8 @@ public class CrimeLab {
         return sCrimeLab;
     }
     private CrimeLab(Context context){
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
         mCrimes = new ArrayList<>();
         for(int i = 0 ; i< 100 ;i++){
             Crime crime = new Crime();
---
+++ app/src/main/java/com/saracawley/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,17 @@
+package com.saracawley.criminalintent.database;
+
+/**
+ * Created by sara on 4/7/2016.
+ */
+public class CrimeDbSchema {
+    public static final class CrimeTable{
+        public static final String NAME = "crimes";
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+
+        }
+    }
+}
